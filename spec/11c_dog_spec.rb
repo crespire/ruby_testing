b97bc6228185d33251c6a6ad8637598a881eea3a
crespire <li.simmon@gmail.com>
2021-12-01 20:15:03 -0500

    Complete ex 11

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
+  subject(:halle) { described_class.new('Halle', 'Cunucu', 'tan') }
 
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'shared method, talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(halle).not_to be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(halle).to be_sleeping
   end
 end
