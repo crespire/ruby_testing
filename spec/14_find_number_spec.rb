a633a502dbe19badead75f20e0efae4d3cf4b791
crespire <li.simmon@gmail.com>
2021-12-03 20:10:21 -0500

    Complete ex 14

--- spec/14_find_number_spec.rb
+++ spec/14_find_number_spec.rb
@@ -230,20 +230,24 @@ describe FindNumber do
     context 'when the guess is less than the answer' do
       subject(:low_guess_game) { described_class.new(0, 9, number_range, 4) }
 
-      xit 'updates min to 5' do
+      it 'updates min to 5' do
+        expect { low_guess_game.update_range }.to change { low_guess_game.min }.from(0).to(5)
       end
 
-      xit 'does not update max' do
+      it 'does not update max' do
+        expect { low_guess_game.update_range }.not_to(change { low_guess_game.max })
       end
     end
 
     context 'when the guess is more than the answer' do
       subject(:high_guess_game) { described_class.new(0, 9, number_range, 9) }
 
-      xit 'does not update min' do
+      it 'does not update min' do
+        expect { high_guess_game.update_range }.not_to(change { high_guess_game.min })
       end
 
-      xit 'updates max to 8' do
+      it 'updates max to 8' do
+        expect { high_guess_game.update_range }.to change { high_guess_game.max }.from(9).to(8)
       end
     end
 
@@ -259,10 +263,13 @@ describe FindNumber do
     # Write a test for any 'edge cases' that you can think of, for example:
 
     context 'when the guess is 7, min=5, and max=8' do
-      xit 'updates min to the same value as max' do
+      subject(:edge_guess_game) { described_class.new(5, 8, number_range, 7) }
+      it 'updates min to the same value as max' do
+        expect { edge_guess_game.update_range }.to change { edge_guess_game.min }.from(5).to(8)
       end
 
-      xit 'does not update max' do
+      it 'does not update max' do
+        expect { edge_guess_game.update_range }.not_to(change { edge_guess_game.max })
       end
     end
   end
